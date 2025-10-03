0a2514f41fad4a27b31e3774e06d261ecbe9f984
Tobi Francis Ogounz <77078820+TobbyFranc@users.noreply.github.com>
2025-10-03 11:37:34 -0700

    Fix Login import path for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import { Routes, Route } from "react-router-dom"
 import LandingPage from "./components/LandingPage"
-import Login from "./components/LogIn"
+import Login from "./components/Login"
 import Signup from "./components/Signup"
 import Dashboard from "./components/Dashboard"
 import LocationSearch from "./components/LocationSearch"
